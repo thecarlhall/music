d812afeae8b0d179fcfe0158684da5179abf4fbe
Carl Hall <carl.hall@gmail.com>
2017-01-09 23:18:01 -0800

    Add blues hexatonic scale

--- src/main/java/thecarlhall/music/Modes.java
+++ src/main/java/thecarlhall/music/Modes.java
@@ -9,7 +9,8 @@ public enum Modes {
     AEOLIAN(6, new int[]{2, 1, 2, 2, 1, 2, 2}),
     LOCRIAN(7, new int[]{1, 2, 2, 1, 2, 2, 2}),
     MAJOR_PENTA(1, new int[]{2, 2, 3, 2, 3}),
-    MINOR_PENTA(1, new int[]{3, 2, 2, 3, 2});
+    MINOR_PENTA(1, new int[]{3, 2, 2, 3, 2}),
+    BLUES(1, new int[]{3, 2, 1, 1, 3, 2});
 
     public final int tonic;
     public final int[] intervals;
